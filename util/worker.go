5e9a73c176bbf4f78bc53ec77fae4ea9e81e090c
Loai Esam <loaie1199@gmail.com>
2023-06-15 20:03:02 +0300

    Replace Request.go with Worker.go

---
+++ util/worker.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+func Worker(
+	id int,
+	wg *sync.WaitGroup,
+	DomainNames []string,
+	timeout int,
+	InsecureSkipVerify bool,
+	mutex *sync.Mutex,
+	file *os.File) {
+	// defer to mark the task as done when the function finishes
+	defer wg.Done()
+	fmt.Println("worker: ", id, "started")
+	// perform some task
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: InsecureSkipVerify,
+		},
+	}
+
+	// Acquire the lock before writing to the file
+	mutex.Lock()
+	defer mutex.Unlock()
+	// make a seperate client to not interfere with other requests while multiprocessing
+	client := http.Client{Transport: tr, Timeout: time.Duration(timeout) * time.Second}
+	for _, DomainName := range DomainNames {
+		resp, err := client.Get(DomainName)
+		if err != nil {
+			continue
+		} else if resp.StatusCode == 200 {
+			_, err := file.Write([]byte(DomainName))
+			if err != nil {
+				fmt.Printf("Error writing to file: %v\n", err)
+				return
+			}
+		}
+		defer resp.Body.Close()
+	}
+	fmt.Println("worker", id, "finished")
+}
